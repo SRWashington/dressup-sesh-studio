88c56a626a965b173658ef16c6430e0f2ef7f649
SRWashington <jcrump15@yahoo.com>
2026-08-18 17:05:01 -0700

    Connect Supabase project

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DRESSUP_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://zlcjrwhdtrhtobmgpafw.supabase.co",
+  supabasePublishableKey: "sb_publishable_BS4jfizO_-F3ZpUvuSSyyw_bMH1sG-D"
 };
